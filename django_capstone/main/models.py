0fe91558be65dd0caeea1329a46c6a536d753198
hyunjaelee <leehj8687@gmail.com>
2019-04-06 23:15:05 +0900

    Google login to User(db)

--- django_capstone/main/models.py
+++ django_capstone/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class UserManager(models.Manager):
+#     use_related_fields = True
+
+#     def new(self, **kwargs):
+#         return self.filter(pub_date__lte=timezone.now(), **kwargs)
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    # objects = UserManager()
--- django_capstone/main/views.py
+++ django_capstone/main/views.py
@@ -2,13 +2,31 @@ from django.shortcuts import render
 from django.http import *
 from django.contrib import auth
 from django.shortcuts import redirect
-
+from .models import User
 # 썸네일 이미지를 얻기 위해 추가
 import requests
 import json
 
 
 def index(request):
+    if(len(request.user.username) > 0):
+        print(User.objects.values('user_name'))
+        checklist = list(User.objects.values('user_name'))
+        token = False
+
+        # check if the account is already created or not.
+        for element in checklist:
+            if element['user_name'] == request.user.username:
+                # 이미 생성된 계정임.
+                print("it is detected")
+                token = True
+                break
+        if(token is False):
+            new_user = User(user_name=request.user.username)
+            new_user.save()
+            print("new user")
+
+    # User.objects.new.create(user_name=request.user.username)
     return render(request, 'home/index.html')
 
 
